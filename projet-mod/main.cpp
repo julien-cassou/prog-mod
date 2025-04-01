bdaa6f7dcb30bc72b1e0db80ebf381dfee34f6b9
luciacasalta <luciacasaltap@gmail.com>
2025-04-01 14:40:25 +0200

    projet.cpp
    TEST NUMERO 2!!!

---
+++ projet-mod/main.cpp
@@ -0,0 +1,14 @@
+//
+//  main.cpp
+//  projet.cpp
+//
+//  Created by Lucia Casalta on 01/04/2025.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
